c1058baa961e460795d8016ff4f88b1f9b8aa82b
Konstantin Tokarev <annulen@yandex.ru>
2020-02-21 06:46:13 +0300

    Sync files with qt5/5.14

--- coin/provisioning/common/windows/conan.ps1
+++ coin/provisioning/common/windows/conan.ps1
@@ -1,3 +1,37 @@
+#############################################################################
+##
+## Copyright (C) 2019 The Qt Company Ltd.
+## Copyright (C) 2019 Konstantin Tokarev <annulen@yandex.ru>
+## Contact: http://www.qt.io/licensing/
+##
+## This file is part of the provisioning scripts of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:LGPL21$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see http://www.qt.io/terms-conditions. For further
+## information use the contact form at http://www.qt.io/contact-us.
+##
+## GNU Lesser General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU Lesser
+## General Public License version 2.1 or version 3 as published by the Free
+## Software Foundation and appearing in the file LICENSE.LGPLv21 and
+## LICENSE.LGPLv3 included in the packaging of this file. Please review the
+## following information to ensure the GNU Lesser General Public License
+## requirements will be met: https://www.gnu.org/licenses/lgpl.html and
+## http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+##
+## As a special exception, The Qt Company gives you certain additional
+## rights. These rights are described in The Qt Company LGPL Exception
+## version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
 . "$PSScriptRoot\helpers.ps1"
 
 $scriptsPath = "C:\Python36\Scripts"
@@ -40,8 +74,8 @@ function Run-Conan-Install
         $extraArgs += " -s compiler.threads=$CompilerThreads"
     }
 
-    $manifestsDir = "${env:APPVEYOR_BUILD_FOLDER}\conan_manifests\conan_manifests"
-    $buildinfoRoot = "${env:APPVEYOR_BUILD_FOLDER}\install"
+    $manifestsDir = "$PSScriptRoot\conan_manifests"
+    $buildinfoRoot = "C:\Utils\conanbuildinfos"
 
     Get-ChildItem -Path "$ConanfilesDir\*.txt" |
     ForEach-Object {
@@ -52,7 +86,7 @@ function Run-Conan-Install
         for ($i = 1; $i -le 5; $i++) {
             try {
                 Push-Location $outpwd
-                Run-Executable "$scriptsPath\conan.exe" "install --manifests $manifestsDir", `
+                Run-Executable "$scriptsPath\conan.exe" "install --no-imports --verify $manifestsDir", `
                     '-s', ('compiler="' + $Compiler + '"'), `
                     "-s os=Windows -s arch=$Arch -s compiler.version=$CompilerVersion $extraArgs $conanfile"
                 break;

2ad2087e8b1e69063b5cb0d32ba00754494c4b23
Konstantin Tokarev <annulen@yandex.ru>
2020-09-11 16:31:19 +0300

    sync_code.sh should sync conan_requirements.txt as well

--- sync_code.sh
+++ sync_code.sh
@@ -3,6 +3,7 @@ source $(dirname "$0")/libsync.sh
 sync_files \
     coin/provisioning/common/windows/helpers.ps1 \
     coin/provisioning/common/windows/conan.ps1 \
+    coin/provisioning/common/windows/conan_requirements.txt \
     coin/provisioning/qtci-windows-10-x86/11-conan.ps1 \
     coin/provisioning/qtci-windows-10-x86_64/11-conan.ps1 \
     coin/provisioning/qtci-windows-7-x86/11-conan.ps1
